b82a231529f64aa6ac6cfebe88d3f7025b709c44
AnnabethS <annabeths111@gmail.com>
2022-02-08 15:23:53 +0000

    finished mersennePrime

--- blk2/Sols.hs
+++ blk2/Sols.hs
@@ -62,10 +62,15 @@ doubleOddM Nothing = Nothing
 doubleOddM x | odd (fromJust x) = Just ((fromJust x) *2)
              | otherwise = Nothing
              
+doubleOddM'' :: Maybe Int -> Maybe Int
+doubleOddM'' Nothing = Nothing
+doubleOddM'' (Just x) | odd x = Just (x*2)
+                      | otherwise = Nothing
+
 maybeDouble :: Maybe Int -> Maybe Int
 maybeDouble x = maybe Nothing (Just . (*2)) x
 
-             
+
 doubleOddM'   :: Maybe Int -> Maybe Int
 doubleOddM' x = maybe Nothing (f) x
  where f y | odd y = Just (y * 2)
@@ -104,7 +109,7 @@ base = 0
 recursive = nats ++ [last+1]
 -}
 
--- Q3.3
+-- Q3.3 TODO ask about fix
 
 fix :: (a->a) -> a
 fix    f       = f (fix f)
@@ -112,3 +117,135 @@ fix    f       = f (fix f)
 ones' :: [Integer]
 ones' = fix (1:)
 
+nats' :: [Integer]
+nats' = fix (\x -> 0 : map (+1) x)
+
+-- Q3.5
+
+findPlateau :: Eq a => [a] -> a
+findPlateau (x1:x2:xs) | x1==x2 = x1
+                       | otherwise = findPlateau (x2:xs)
+
+tz :: Int -> Int
+tz    n = negate (n `div` 2)
+
+mystery3_5 :: Eq a => (a -> a) -> a -> a
+mystery3_5 = (findPlateau .) . iterate
+
+
+-- Q3.6
+
+mersenne :: [Int]
+mersenne = [x | x <- [0..], f x 1]
+  where f y p | (2^p) <= y = f y (p+1)
+              | (2^p) == (y+1) = True
+              | otherwise = False
+
+mersenne' :: [Int]
+mersenne' = [(2^x) - 1 | x <- [1..]]
+
+eratosthenes :: [Int]
+eratosthenes = sieve (map Just [2..])
+  where
+    sieve (Just n  : ns) = n : sieve (zipWith ($) mark_n ns)
+      where mark_n = cycle (replicate (n-1) id ++ [const Nothing]) 
+    sieve (Nothing : ns) = sieve ns
+
+
+mersennePrime :: [Int]
+mersennePrime = [x | x <- [0..], (f x mersenne') && (f x eratosthenes)]
+  where f x (l:ls) | x > l = f x ls
+                   | x == l = True
+                   | otherwise = False
+
+-- go up the lists of mersenne and prime, and stop checking a list if
+-- x == the current or x > the current. If x is in both lists, accept it
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+-- Q4: Lazy Trees
+
+newtype Tree a = Tree [(a, Tree a)] deriving Show
+
+data VM_Event = Coin | Choc | Fudj | Halt deriving (Eq, Show)
+
+type VM = Tree VM_Event
+
+vm1_2 :: VM
+vm1_2  = Tree [(Coin, Tree [(Choc, Tree [(Coin, Tree [(Choc, Tree [])])])])]
+
+vm1_e :: VM
+vm1_e = Tree [(Coin, Tree [(Choc, vm1_e)])]
+
+stop :: Tree a
+stop = Tree []
+
+leadsto :: a -> Tree a -> Tree a
+e `leadsto` b = Tree [(e, b)]
+
+branch :: Tree a -> Tree a -> Tree a
+Tree ts `branch` Tree us = Tree (ts ++ us)
+
+vm1_2' :: VM
+vm1_2' = Coin `leadsto` (Choc `leadsto` (Coin `leadsto` (Choc `leadsto` stop)))
+
+vm1_e' :: VM
+vm1_e' = Coin `leadsto` (Choc `leadsto` vm1_e')
+
+vm1_h :: VM
+vm1_h = (Coin `leadsto` ((Choc `leadsto` vm1_h)
+                         `branch`
+                         (Halt `leadsto` stop)))
+        `branch`
+        (Halt `leadsto` stop)
+
+vm1_h' :: VM
+vm1_h' = (Coin `leadsto` (Choc `leadsto` vm1_h))
+         `branch`
+         (Halt `leadsto` stop)
+
+-- Q4.3
+--takeTree :: Int -> Tree a -> Tree a
+--takeTree 0 (Tree (t, _)) = t
+--takeTree d (Tree (t, ts)) = Tree (t, (takeTree (d-1) ts)) 
+  
+
+-- Q4.4
+  
+
+  
+-- Q5
+
+{-
+tic tac toe
+-}
+
+newtype Grid = Grid (Int -> Int)
+-- 0 is not played
+-- 1 is p1 (Os)
+-- 2 is p2 (Xs)
+
+f :: Int -> Int
+f x = x+1
+
+g :: Int -> Int
+g x = x * 2
+
+h :: Int -> Int
+h = f . g
