0ccbda971816a4557629c61e9ab6116e938075f8
Anna <annabeths111@gmail.com>
2022-05-13 20:03:21 +0100

    added new tree test

--- Sandbox.hs
+++ Sandbox.hs
@@ -5,3 +5,27 @@ cmbProd = zipWith (*)
 
 cmbList :: [a] -> [a] -> [a]
 cmbList xs ys = concat (zipWith (\ x y -> [y,x]) xs ys)
+
+data BinTree x = Lf Int | Branch (BinTree x) x (BinTree x)
+  deriving (Eq, Show)
+
+testTree :: BinTree Char
+testTree = Branch (Branch (Branch (Lf 1) 'd' (Lf 2)) 'b' (Lf 3)) 'a' (Branch (Lf 4) 'c' (Branch (Lf 5) 'g' (Lf 6)))
+
+{-
+         a
+        / \
+       /   \
+      b     c
+     / \   / \
+    d   3 4   g
+   / \       / \
+  1   2     5   6
+-}
+
+
+isTreeBal :: BinTree a -> Bool
+isTreeBal (Lf _) = True
+isTreeBal (Branch a _ b) = isTreeBal a && isTreeBal b && getDepth a == getDepth b
+  where getDepth (Lf x) = x
+        getDepth (Branch c _ d) = max (getDepth c) (getDepth d)
